f2aa116b9673192e109b3834fed5940b8f3ef550
Alessandro Scolozzi <scolozzi.alessandro@gmail.com>
2022-06-16 11:28:24 +0200

    first commit

---
+++ sintassi-e-return-implicito/js/script.js
@@ -0,0 +1,9 @@
+// const sumNumbers = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// Con return implicito
+const sumNumbers = (num1, num2) => num1 + num2;
+
+const miaSomma = sumNumbers(10, 6);
+console.log(miaSomma);
